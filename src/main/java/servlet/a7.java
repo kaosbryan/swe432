eb0d42360668be836295670bbb9a97e7049bc60a
Kaostft135 <kaosbryan99@gmail.com>
2020-04-14 14:03:48 -0400

    a7 errors

--- src/main/java/servlet/a7.java
+++ src/main/java/servlet/a7.java
@@ -18,31 +18,17 @@ public class a7 extends HttpServlet{
   @Override
    protected void doPost (HttpServletRequest req, HttpServletResponse res)
           throws ServletException, IOException{
-       PrintWriter out = response.getWriter();
+        PrintWriter out = response.getWriter();
+        String element = "";
 		out.print("<html>\n<head>\n\n");
 		out.print("<title>SWE 432 Bryan Zheng</title>\n");
 		out.print("</head>\n");
 		out.print("<body>\n");
 		out.print("<center><h2>Bryan Zheng  ------- Bathroom Review Form</h2></center>\n");
 		out.print("<hr>\n");
-		String bF = request.getParameter("building");
-		String fF = request.getParameter("floor");
-		String cF = request.getParameter("clean");
-		String tF = request.getParameter("traffic");
-		out.print("The building is ");
-		out.print(bF);
-		out.print(" on Floor ");
-		out.print(fF);
-		out.print(". ");
-		out.println("This bathroom is");
-		out.print(cF);
-		out.print(" and ");
-		out.print(tF);
-		out.print(".");
-		out.print("\n");
-		out.print("</body>\n");
-		out.print("</html>\n");
-		out.close();
-		out.close ();
+		while (paraNames.hasMoreElements())
+		    Enumeration paraNames = request.getParameterNames();
+		    element = (String)paraNames.nextElement();
+		    out.print(element)
     }
 }
