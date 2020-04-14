d99291cab92dde5fab53c6bc28642a0d20e0bcdb
Kaostft135 <kaosbryan99@gmail.com>
2020-04-14 14:34:15 -0400

    a7 errors

--- src/main/java/servlet/a7.java
+++ src/main/java/servlet/a7.java
@@ -49,7 +49,7 @@ public class a7 extends HttpServlet{
                     {
                     out.print("  <tr>");
                     out.print("    <td style=\"width: 20%\" width=\"20%\">&nbsp;</td>");
-                    out.print("    <td>" + values[i] + "</td></tr>");
+                    out.print("    <td>" + values[i] + "</td></tr>\n");
                     }
                 }
             }
